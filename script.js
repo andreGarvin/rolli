cc3ed1c009a1125fa0955679adca6f009cb71c9d
andreGarvin <andregarvin718@gmail.com>
2017-02-24 22:24:03 -0500

    commented heck out of the webser and the script file for rolli, also added the fs module for the group JSON as the "fake_db".

--- script.js
+++ script.js
@@ -3,32 +3,36 @@ const date = new Date;
 const app = new Vue({
     el: '#app',
     data: {
-        socket: io(),
-        msg: '',
-        query: '',
-        gif_query: '',
-        seen: true,
-        groupbin: false,
-        resp: null,
+        socket: io(),    // the socket method/initilizing
+        msg: '',         // holds the value of 'msg'
+        query: '',       // holds the value for 'query'
+        seen: true,      // the 'seen' variabelfor displaying things on the website
+        groupbin: false, // displays the group settings panel
+        resp: null,      // holds the resp from the backend webserver
+        // displaying the cureent tima and date.
         curr_date: `${ date.getMonth() }/${ date.getDate() }/${ date.getFullYear() } ${ date.getHours() - 12 }:${ date.getMinutes() }`,
         gihpy: {
-            gifs: [],
+            gif_query: '',   // holds the value for the 'gihpy.gif_query'
+            gifs: [],        // array of results from gihpy API in 'gifs'
         },
+        // the user info/settings
         user: {
-            listedgroups: ['global'],
-            user_name: 'andreGarvin',
+            listedgroups: Object.keys( this.user.groups ),    // listed groups the user is in
+            user_name: '',   // 'user_name' of the user in the chat room
+            // manging the chat channels the user is on
             session: {
-              group: 'global',
-              key: 'meme_cats'
+              group: 'global',   // group name
+              key: 'meme_cats'   // group key
             },
             groups: {
+              // default group chat
               global: {
-                    name: 'global',
-                    k: 'meme_cats',
-                    msgs: [],
-                    attachments: [],
-                    members: [],
-                    active_users: []
+                    name: 'global',  // group name
+                    k: 'meme_cats',  // group key
+                    msgs: [],       // group msgs
+                    attachments: [],  // group attachments
+                    members: [],      // group memebers
+                    active_users: []  // groups active users
                   }
               }
         }
@@ -42,15 +46,21 @@ const app = new Vue({
                loop starts and to be assinged later */
             var user_name = '';
 
+            // if the user does given in data keep prmpting for a username
             while ( user_name.length === 0 ) {
-                var user_name = prompt('eneter user name');
+                   var user_name = prompt('eneter user name');
             }
 
+            // if the user gives a 'user_name'
+               // assign it to the 'this.user.user_name' data
             this.user.user_name = user_name;
+            // emit a message to the backend
             this.socket.emit('join', { 'user_name': user_name, groups: Object.keys( this.user.groups ), whisper: [] });
 
+            // wait for a message to come back from the backend
             this.socket.on('join', function( msg ) {
 
+                 // print to the console that message
                  console.log( msg );
             });
         },
@@ -58,17 +68,23 @@ const app = new Vue({
         // sending message to otherin channel
         send_msg: function() {
 
+            // if the user 'this.msg' is not empty send it
             if ( this.msg.length !== 0 ) {
 
+                // however if the perosn sends a whisper
+                // then do not print to the main stream
                 if ( this.msg.slice(0, 2) === 'w/' ) {
 
+                    // emit to backend
                     this.socket.emit(this.user.session.group,{ type: 'whisper', recp: this.user.user_name, recv: this.msg.split(' ')[1].split(':')[0], msg: this.msg.split(' ').slice(2, this.msg.length).join(' '), time: this.curr_date, session: this.user.session });
                 }
                 else {
 
+                    // else if it is a reggular message then print/append to the main stream
                     this.socket.emit(this.user.session.group, { type: 'text', msg: this.msg, time: this.curr_date, user_name: this.user.user_name, session: this.user.session });
                 }
 
+                // empty the 'msg' data to a empty string
                 this.msg = '';
             }
 
@@ -77,6 +93,7 @@ const app = new Vue({
         // recving messages
         recv_msg: function() {
 
+            // recv message from the channel/session the user in on
             this.socket.on(this.user.session.group, function( msg ) {
 
                 // finish whisper function
@@ -92,34 +109,44 @@ const app = new Vue({
                     // }
                 }
                 else {
+
+                      // recv the message from bakend
                       this.user.groups[this.user.session.group].msgs.push( msg );
                 }
-            }.bind(this));
+             }.bind(this));
 
         },
 
         // loding gifs
         load_gifs: function() {
 
-              this.gif_query = '';
+              // set the 'gihpy.gif_query' to a empty string
+              this.gihpy.gif_query = '';
 
-              if ( this.gihpy.gifs.length === 0 ){
+              // check if the 'gihpy.gif_query' does not equeal a empty string
+              if ( this.gihpy.gifs.length === 0 ) {
 
-
+                  // decalare a app varibel that hldet the 'this' key word
                   var app = this;
 
-
+                  // make a GET request to the gihpy API
                   axios.get('https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
                       .then(function( resp ) {
+                          // assign the 'resp.data' to variable
                           var giphy_resp = resp.data;
 
+                          // iterate over the 'data' in the resp from gihpy API
                           for ( var j in giphy_resp.data ) {
-                              if ( giphy_resp.data[j].images.original.url !== undefined ) {
-      	                       app.gihpy.gifs.push( giphy_resp.data[j].images.fixed_width_small.url );
-                              }
+
+                               // check if the 'src'/url does not equal a empty string or not avaible
+                               if ( giphy_resp.data[j].images.original.url !== undefined ) {
+
+                                   //  append the results that are not undfined to the 'gihpy.gifs' array
+                                   app.gihpy.gifs.push( giphy_resp.data[j].images.fixed_width_small.url );
+                                }
                           }
                       });
-              }
+                }
           },
 
             // sending gifs to other on the channel
@@ -127,18 +154,20 @@ const app = new Vue({
 
                 this.socket.emit(this.user.session.group, { type: 'gif', src: src, time: '12:44 pm', user_name: this.user.user_name });
                 this.gihpy.gifs = [];
-                this.gif_query = '';
+                this.gihpy.gif_query = '';
+
             },
 
             clearscreen: function() {
 
-              this.groupbin = false;
-              this.gihpy.gifs = [];
+                 this.groupbin = false;
+                 this.gihpy.gifs = [];
+
             },
 
             switchSessionChannel: function( groupName ) {
 
-              this.user.session.group = groupName;
+                 this.user.session.group = groupName;
             },
 
             createGroup: function() {
@@ -215,58 +244,70 @@ const app = new Vue({
     watch: {
         msg: function() {
 
+            // sends url to backend
             function sendURL( url ) {
 
-              this.socket.emit(this.user.group, { type: 'url', link: url, user_name: this.user.user_name, time: '12:44 pm'} );
-              return true;
+                 this.socket.emit(this.user.group, { type: 'url', link: url, user_name: this.user.user_name, time: '12:44 pm'} );
+                 return true;
             }
 
+            // sends images to backend
             function sendIMAGE( src ) {
 
-              this.socket.emit(this.user.session.group, { type: 'src', 'src': src, user_name: this.user.user_name, time: '12:44 pm' });
-              return true;
+                 this.socket.emit(this.user.session.group, { type: 'src', 'src': src, user_name: this.user.user_name, time: '12:44 pm' });
+                 return true;
             }
 
             if ( this.msg.slice(0, 8) === 'https://' || this.msg.slice(0, 7) === 'http://' ) {
-              sendURL( msg );
+                sendURL( msg );
             }
             else if ( this.msg.slice( 58, this.msg.length ) === '.jpg' || this.msg.slice( 58, this.msg.length ) === '.png' || this.msg.slice(59, this.msg.length) === '.jpg' || this.msg.slice(59, this.msg.length) === '.png'  ) {
-              sendIMAGE( this.msg );
+                 sendIMAGE( this.msg );
             }
 
         },
         query: function() {
-            if ( this.query.length < 0 || this.query.length !== 0 ) {
-                this.search( this.query, this.user.user_name );
-            }
+
+              if ( this.query.length < 0 || this.query.length !== 0 ) {
+
+                  this.search( this.query, this.user.user_name );
+              }
+
         },
 
         // continous reloading and searching for gifs
         gif_query: function() {
-            if ( this.gif_query.length < 0 || this.gif_query.length !== 0 ) {
+            if ( this.gihpy.gif_query.length < 0 || this.gihpy.gif_query.length !== 0 ) {
+
                 var app = this;
 
-                axios.get(`https://api.giphy.com/v1/gifs/search?q=${ app.gif_query}&api_key=dc6zaTOxFJmzC`)
+                axios.get(`https://api.giphy.com/v1/gifs/search?q=${ app.gihpy.gif_query}&api_key=dc6zaTOxFJmzC`)
                     .then(function( resp ) {
-                        app.gihpy.gifs = [];
 
-                        var giphy_resp = resp.data;
+                          app.gihpy.gifs = [];
 
-                        for ( var j in giphy_resp.data ) {
-                            if ( giphy_resp.data[j].images.original.url !== undefined ) {
-    	                       app.gihpy.gifs.push( giphy_resp.data[j].images.fixed_width_small.url );
-                            }
-                        }
+                          var giphy_resp = resp.data;
+
+                          for ( var j in giphy_resp.data ) {
+
+                              if ( giphy_resp.data[j].images.original.url !== undefined ) {
+
+                                  app.gihpy.gifs.push( giphy_resp.data[j].images.fixed_width_small.url );
+                               }
+                           }
 
                     });
             }
             else {
-                this.gihpy.gifs = [];
 
-                this.load_gifs();
+                 this.gihpy.gifs = [];
+                 this.load_gifs();
             }
+
         }
+
     }
+
 });
 
 // init the connection
